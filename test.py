9f08e7f72dbe91c36487849d28d53b60feb3c803
ksonod <44776642+ksonod@users.noreply.github.com>
2019-02-01 12:21:43 +0100

    Update test.py

--- test.py
+++ test.py
@@ -2,5 +2,5 @@ import plotdata as pltdt
 
 #You should prepare your text file and specify a path.
 #example: 'C:\\Users\\S\\Documents\\python\\test.txt'
-path= 'C:\\Users\\Kotaro Sonoda\\Documents\\python\\test.txt'
+path= 'C:\\Users\\S\\Documents\\python\\test.txt'
 pltdt.plot2Ddata(path)
